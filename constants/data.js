99138f5dc4c191c278ba7e21eb8392c1316276ad
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-04-06 21:37:11 +0100

    Add about page page

--- constants/data.js
+++ constants/data.js
@@ -1,19 +1,30 @@
 const userData = {
-    githubUsername: "Yorsyboy",
-    name: "Toyosi Taiwo",
-    designation: "Frontend/Full-Stack Developer",
-    avatarUrl: "/avatar.jpg",
-    email: "adekunletaiwo43@yahoo.com",
-    phone: "+234 8183134563",
-    address: "Lagos, Nigeria",
-    socialLinks: {
-        instagram: "https://www.instagram.com/yorsyboy/",
-        twitter: "https://twitter.com/Tboytaiwo",
-        linkedin: "https://www.linkedin.com/in/taiwo-toyosi/",
-        github: "https://github.com/Yorsyboy",
-        facebook: "https://facebook.com/",
-        frontendmentor: "https://www.frontendmentor.io/profile/Yorsyboy",
-      },
+  githubUsername: "Yorsyboy",
+  name: "Toyosi Taiwo",
+  designation: "Frontend/Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "adekunletaiwo43@yahoo.com",
+  phone: "+234 8183134563",
+  address: "Lagos, Nigeria",
+  socialLinks: {
+    instagram: "https://www.instagram.com/yorsyboy/",
+    twitter: "https://twitter.com/Tboytaiwo",
+    linkedin: "https://www.linkedin.com/in/taiwo-toyosi/",
+    github: "https://github.com/Yorsyboy",
+    facebook: "https://facebook.com/",
+    frontendmentor: "https://www.frontendmentor.io/profile/Yorsyboy",
+  },
+  about: {
+    title:
+      "A Software Developer passionate about building web applications with JavaScript, React and Next. I fell in love with programming ever since I have been building projects with the stacks mentioned above. I am currently looking for new opportunities within a technical team.",
+    description: [
+      `I am a passionate software developer who has a flair for UI design, I recently graduated from Microverse, a remote software development company, where I learnt to pair-program and build projects.`,
+      `I have a a passion for user interactive designs and I have spent the last months completely immersed in remote development, building everything from landing pages to Rails APIs`,
+      `Apart from coding, some other activities that I love to do are playing games, playing football, watching animes and movies`,
+    ],
+  },
+    resumeUrl:
+    "https://docs.google.com/document/d/1JNTeudeZ_EXPw4Io9DowYAfJo0PtbG31jTyrS6vhfT8/edit?usp=sharing",
 }
 
 export default userData;
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,236 @@
+import React from "react";
+import userData from "../../constants/data";
+
+export default function AboutMe() {
+  // Age calculator function
+  function age(birthdate) {
+    const today = new Date();
+    const age =
+      today.getFullYear() -
+      birthdate.getFullYear() -
+      (today.getMonth() < birthdate.getMonth() ||
+        (today.getMonth() === birthdate.getMonth() &&
+          today.getDate() < birthdate.getDate()));
+    return age;
+  }
+
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div>
+        <h1 className="text-4xl text-gray-500 dark:text-white text-center font-bold">
+          Who am I ?
+        </h1>
+
+        <div className="h-screen relative text-gray-800 dark:text-white">
+          <div className="h-full">
+            <lottie-player
+              src="https://assets10.lottiefiles.com/packages/lf20_z7eBBeOq5L.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">
+              Hi there, I'm <hr />
+              <pre className="text-xl md:text-sm my-5 font-mont font-semibold">
+                {JSON.stringify(
+                  {
+                    name: userData.name,
+                    age: age(new Date(1998, 10, 10)),
+                    gender: "Male",
+                    country: "Nigeria",
+                  },
+                  null,
+                  2
+                )}
+              </pre>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800 -mt-20">
+        <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
+          About Me.
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            {userData.about.title}.
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20 ">
+          {/* Social Buttons */}
+          <div className="inline-flex flex-col mb-20">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                For any sort help / enquiry, shoot a{" "}
+                <a
+                  href={`mailto:${userData.email}`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  mail
+                </a>{" "}
+                and I'll get back. I swear.
+              </p>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Job Opportunities
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                I'm looking for a job currently, If you see me as a good fit,
+                check my{" "}
+                <a
+                  href={userData.resumeUrl}
+                  target="__blank"
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  CV
+                </a>{" "}
+                and I'd love to work for you.
+              </p>
+            </div>
+            {/* Social Links */}
+            <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
+              Social Links
+            </h1>
+            <div className="mt-4 ml-4">
+              <div className="flex flex-row justify-start items-center ">
+                <a
+                  href={userData.socialLinks.frontendmentor}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    FrontEnd Mentor
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.twitter}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Twitter
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.github}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    GitHub
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.linkedin}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    LinkedIn
+                  </div>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={userData.socialLinks.instagram}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300"></div>
+                    Instagram
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Text area */}
+          <div className="col-span-1 md:col-span-2">
+            {userData.about.description?.map((desc, idx) => (
+              <p
+                key={idx}
+                className="text-xl text-gray-700 mb-4 dark:text-gray-300 "
+              >
+                {desc}
+              </p>
+            ))}
+
+            <h1 className="bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50">
+              Tech Stack
+            </h1>
+            <div className="flex flex-row flex-wrap mt-8">
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/tailwind/tailwind.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://d2nir1j4sou8ez.cloudfront.net/wp-content/uploads/2021/12/nextjs-boilerplate-logo.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+              <img
+                src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
+                className="h-20 w-20 mx-4 my-4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
