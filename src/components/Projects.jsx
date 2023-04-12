feaa510ca97142bae349666f337168adaf4c0214
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-04-12 20:08:12 +0100

    fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -87,7 +87,7 @@ export default function FavouriteProjects() {
                 className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Space Traveler's Hub
+                Space Traveler&apos;s Hub
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 03
