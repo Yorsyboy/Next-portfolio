ccb00902680774f51fef4cadbbb82cf3d4bb3e6f
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-04-20 09:25:59 +0100

    Add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute='class'>
       <div className='dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-100 min-h-screen'>
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ThemeProvider>
   )
